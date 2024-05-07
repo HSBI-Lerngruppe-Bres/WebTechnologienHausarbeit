d8a403b768b90bfd3acec085e1c967de71d0d30c
Nils Baxheinrich <nils@nbax.de>
2024-05-07 11:18:15 +0200

    feat: create basic flask app

---
+++ bruno/__init__.py
@@ -0,0 +1,61 @@
+import os
+import logging
+from flask import Flask
+#from flask_login import LoginManager
+from pathlib import Path
+
+
+def create_app():
+    logging.basicConfig(level=logging.INFO)
+    logging.info(f"Creating app")
+    
+    INSTANCE_PATH = os.path.abspath(os.path.join(
+        os.path.abspath(__path__[0]), "../instance"))
+    app = Flask(__name__, instance_path=INSTANCE_PATH)
+    logging.info(f"Created app at: {INSTANCE_PATH}")
+
+    CONFIG_PATH = Path(app.instance_path) / 'config.cfg'
+    if CONFIG_PATH.is_file():
+        app.config.from_pyfile(str(CONFIG_PATH))
+        logging.info(f"Loaded config from: {CONFIG_PATH}")
+
+    else:
+        logging.warning(
+            f"Configuration file not found at {CONFIG_PATH}")   
+        
+    if app.config.get('DEBUG', False):
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.debug(f"App created in debug mode at: {INSTANCE_PATH}")
+        #TODO Handle other Debug stuff
+        #TODO Maybe split into multiple functions and files
+
+
+    """from .database import db
+    db.init_app(app)
+
+    # Setup Flask-Login
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'site.login'
+
+    @login_manager.user_loader
+    def user_loader(user_id):
+        return User.query.get(user_id)
+
+    with app.app_context():
+        db.create_all()
+
+        # Check if every Table is empty
+        NEW_DB = all(db.session.query(table).first()
+                     is None for table in db.metadata.sorted_tables)
+
+        if NEW_DB:
+            logging.info("All tables are empty. Seeding database...")
+            seed_database()"""
+
+    #from .sites import site
+    #app.register_blueprint(site)
+
+    # logging.info(app.config)
+
+    return app
