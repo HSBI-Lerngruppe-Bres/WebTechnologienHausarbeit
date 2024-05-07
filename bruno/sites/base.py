1f0e18cc3acc30f352f1fd4f037e06e83da7aeab
Nils Baxheinrich <nils@nbax.de>
2024-05-07 11:43:37 +0200

    feat: index_site

--- bruno/sites/base.py
+++ bruno/sites/base.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 from .account import site as account_site
 
 site = Blueprint("site", __name__, template_folder="templates", url_prefix="/")
@@ -6,3 +6,4 @@ site.register_blueprint(account_site)
 
 @site.get("/")
 def index():
+    return render_template("index.html")
