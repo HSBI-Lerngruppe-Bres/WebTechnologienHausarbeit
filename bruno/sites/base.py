81f17915b458c6e2a6b92076fe48d4fc4645fb54
Nils Baxheinrich <nils@nbax.de>
2024-05-08 11:43:50 +0200

    chore: remove logout html

--- bruno/sites/base.py
+++ bruno/sites/base.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template
 from .accounts import site as accounts_site
+from flask_login.utils import login_required
 
 site = Blueprint("sites", __name__, template_folder="templates", url_prefix="/")
 site.register_blueprint(accounts_site)
@@ -7,3 +8,13 @@ site.register_blueprint(accounts_site)
 @site.get("/")
 def index():
     return render_template("index.html")
+
+@site.get("/games/")
+@login_required
+def games():
+    games = [
+        {'name': "Mark's Game", 'player_count': 4, 'protection': 2},
+        {'name': "John's Game", 'player_count': 12, 'protection': 1},
+        {'name': "Jane's Game", 'player_count': 6, 'protection': 0}
+    ]
+    return render_template("games.html", games=games)
