fcda06f47014efc89931731873e92e632cbe4732
Nils Baxheinrich <nils@nbax.de>
2024-05-10 23:20:51 +0200

    fix: join game form redirect

--- bruno/sites/base.py
+++ bruno/sites/base.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, current_app
+from flask import Blueprint, render_template, redirect, url_for, current_app, request
 from .accounts import site as accounts_site
 from .game import site as game_site
 from flask_login.utils import login_required, current_user
@@ -23,7 +23,7 @@ def games():
     games = get_active_games()
     create_game_form = CreateGameForm(username=current_user.username)
     join_game_form = JoinGameForm()
-    if create_game_form.validate_on_submit():
+    if 'create_game_submit' in request.form and create_game_form.validate_on_submit():
         game = create_games(create_game_form.game_name.data,
                             create_game_form.public.data,
                             create_game_form.password.data,
@@ -32,7 +32,7 @@ def games():
             "SECRET_KEY"), min_length=5)
         return redirect(url_for('sites.game.join', hashed_game_id=hashids.decode(game.id)))
 
-    if join_game_form.validate_on_submit():
+    elif 'join_game_submit' in request.form and join_game_form.validate_on_submit():
         return redirect(url_for('sites.game.join', hashed_game_id=join_game_form.hashed_game_id.data))
 
     return render_template("games.html", games=games, create_game_form=create_game_form, join_game_form=join_game_form)
