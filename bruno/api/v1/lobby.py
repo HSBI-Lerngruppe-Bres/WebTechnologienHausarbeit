0b535ba7519574f1cb692c860ccb1d24be91bc68
Nils Baxheinrich <nils@nbax.de>
2024-05-12 08:48:39 +0200

    feat: PLayerlist async

--- bruno/api/v1/lobby.py
+++ bruno/api/v1/lobby.py
@@ -1,3 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, current_app, jsonify
+from flask_login import login_required, current_user
+from hashids import Hashids
+from bruno.database.interaction.game import get_players_by_game_id
 
-api = Blueprint('v1', __name__, url_prefix="/lobby")
+api = Blueprint('lobby', __name__, url_prefix="/lobby")
+
+
+@api.route("/state/<string:hashed_game_id>")
+@login_required
+def lobby_state(hashed_game_id):
+    hashids = Hashids(salt=current_app.config.get("SECRET_KEY"), min_length=5)
+    game_id = hashids.decode(hashed_game_id)
+    players = get_players_by_game_id(game_id)
+    players_data = [{'username': player.username, 'id': player.id}
+                    for player in players]
+    game_info = {
+        'game_id': game_id,
+        'players': players_data,
+    }
+    return jsonify(game_info)
--- bruno/sites/game.py
+++ bruno/sites/game.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, url_for, render_template, current_app
-from flask_login import login_required
+from flask_login import login_required, current_user
 from hashids import Hashids
+from bruno.database.interaction.game import player_join_game, get_players_by_game_id
 
 site = Blueprint("game", __name__,
                  template_folder="templates/game", url_prefix="/game")
@@ -13,6 +14,7 @@ def join(hashed_game_id):
     game_id = hashids.decode(hashed_game_id)
     # TODO redirect or password requesr
     # TODO redirect if no game
+    player_join_game(current_user.id, game_id)
     # Logic to add the current user to the game's players
     # Redirect to the game page or display some confirmation message
     return redirect(url_for('sites.game.lobby', hashed_game_id=hashed_game_id))
@@ -23,6 +25,7 @@ def join(hashed_game_id):
 def lobby(hashed_game_id):
     hashids = Hashids(salt=current_app.config.get("SECRET_KEY"), min_length=5)
     game_id = hashids.decode(hashed_game_id)
+    players = get_players_by_game_id(game_id)
     # Logic to add the current user to the game's players
     # Redirect to the game page or display some confirmation message
-    return render_template("lobby.html", hashed_game_id=hashed_game_id)
+    return render_template("lobby.html", hashed_game_id=hashed_game_id, players=players)
